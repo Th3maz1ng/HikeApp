d97500f4a45be614d3fff733ce67cbfe1b51a3b4
anschrammh <bugreport@laposte.net>
2019-06-30 12:05:42 +0200

    ajout du chronometre

---
+++ src/app/home/stopwatch/stopwatch.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, ViewChild, OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'my-stopwatch',
+  providers : [],
+  templateUrl: 'stopwatch.component.html',
+  styleUrls: ['stopwatch.component.scss'],
+  
+})
+
+
+export class StopWatchComponent implements OnInit {
+  
+  constructor() { 
+  }
+  
+  ngOnInit() {
+    
+  }
+}
