bedbbd5a4f4cd1b4a46d750e5f6533aa7b69e725
anschrammh <bugreport@laposte.net>
2019-06-30 12:06:50 +0200

    Integration du chronometre

--- src/app/home/hike.service.ts
+++ src/app/home/hike.service.ts
@@ -10,39 +10,39 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class HikeService {
-  constructor(private http: HttpClient) { }
-  
-  getHikes(): Observable<Hike[]> {
-	var configUrl = 'https://hikes.skyforge.khazad-dum.tech/';
-	var data =this.http.get(configUrl);
-  return this.http.get<Hike[]>(configUrl);
-  }
+	constructor(private http: HttpClient) { }
+	
+	getHikes(): Observable<Hike[]> {
+		var configUrl = 'https://hikes.skyforge.khazad-dum.tech/';
+		var data =this.http.get(configUrl);
+		return this.http.get<Hike[]>(configUrl);
+	}
   
 
-getHikeApi(id:Number) {
-	var configUrl = 'https://hikes.skyforge.khazad-dum.tech/'+id;
-	console.log(this.http.get<Hike>(configUrl))
-  return this.http.get<Hike>(configUrl);
-}
-getHikeGPX(id:Number){
-	var configUrl = 'https://hikes.skyforge.khazad-dum.tech/'+id+"/trace";
-	const options:object ={responseType:'text'};
-
-	return this.http.get<string>(configUrl,options);
-}
+	getHikeApi(id:Number) {
+		var configUrl = 'https://hikes.skyforge.khazad-dum.tech/'+id;
+		console.log(this.http.get<Hike>(configUrl))
+		return this.http.get<Hike>(configUrl);
+	}
+	getHikeGPX(id:Number){
+		var configUrl = 'https://hikes.skyforge.khazad-dum.tech/'+id+"/trace";
+		const options:object ={responseType:'text'};
+	
+		return this.http.get<string>(configUrl,options);
+	}
   
-  htmlMarkGenerator(mark:number)
-  {
-  	let markTab = [];
-  	
-  	for (var i = 0; i < mark; ++i) {
-  		markTab.push(1);
-  	}
-  	
-  	for (var j = 0;j < 5-mark; ++j){
-  		markTab.push(0);
-  	}
-  	
-  	return markTab;
-  }
+	htmlMarkGenerator(mark:number)
+	{
+		let markTab = [];
+		
+		for (var i = 0; i < mark; ++i) {
+			markTab.push(1);
+		}
+		
+		for (var j = 0;j < 5-mark; ++j){
+			markTab.push(0);
+		}
+		
+		return markTab;
+	}
 }
